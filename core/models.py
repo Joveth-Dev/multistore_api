2f8e51b0be1fdf565f896b910a55ab3e85e7e41c
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-02 14:56:03 +0800

    Add initial migration and custom user model with email as username field

--- core/models.py
+++ core/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
 
 
 class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=150,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+    )
     email = models.EmailField(unique=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.get_full_name()
