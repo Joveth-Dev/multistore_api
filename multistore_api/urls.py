7e5bd3f7e9f71681679d3e3e4a6db8f74f414020
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-02 16:29:35 +0800

    Update URL patterns to include JWT authentication and core API routes

--- multistore_api/urls.py
+++ multistore_api/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),
+    path('api/core/', include('core.urls')),
 ]
