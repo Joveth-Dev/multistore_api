43bc8dcb6657a8299e8cfd4e47d8433b1de1e46c
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-23 14:10:25 +0800

    Add management command to create superuser in Docker entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Apply database migrations
 echo "Migrating to database..."
 python manage.py migrate
 
 echo "Collect static files..."
 python manage.py collectstatic --no-input
 
-# Start server
+echo "Executing management command: create_superuser"
+python manage.py create_superuser
+
 echo "Starting server..."
 gunicorn multistore_api.wsgi:application --bind=0.0.0.0:8000 --reload --reload-engine='inotify'
