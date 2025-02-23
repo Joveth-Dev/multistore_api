9dc393115af9b76a0df0eb9a37e74f5e95178908
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-23 14:15:19 +0800

    Add AddressAdmin configuration to admin panel with custom list display

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Store
+from .models import Address, Store
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ["city", "province", "created_at", "updated_at"]
 
 
 @admin.register(Store)
@@ -14,4 +19,3 @@ class StoreAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
-    ordering = ["-updated_at", "-created_at"]
