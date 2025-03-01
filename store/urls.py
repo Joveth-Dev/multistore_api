10f901a41b1aac41428b5c28670599e54efc3182
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-03-02 02:02:25 +0800

    Add ProductViewSet and enhance permissions: register ProductViewSet, implement ownership checks, and update serializer context for product management.

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, StoreViewSet
+from .views import CategoryViewSet, ProductViewSet, StoreViewSet
 
 router = DefaultRouter()
 
 router.register("stores", StoreViewSet, basename="store")
 router.register("categories", CategoryViewSet, basename="category")
-# router.register("products", ProductViewSet)
+router.register("products", ProductViewSet)
 
 urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -3,34 +3,44 @@ from django.db.models.query import Prefetch
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Category, Store
-from .permissions import IsStoreOwner
-from .serializers import CategorySerializer, StoreSerializer
+from .models import Category, Product, Store
+from .permissions import IsCategoryOwner, IsProductOwner, IsStoreOwner
+from .serializers import (
+    CategorySerializer,
+    ListAndRetrieveProductSerializer,
+    ProductSerializer,
+    StoreSerializer,
+)
 
 
 class StoreViewSet(ModelViewSet):
     serializer_class = StoreSerializer
 
     def get_queryset(self):
-        if self.action in ["partial_update", "update"]:
-            user_groups = self.get_user_groups()
-            if "Store Owner" in user_groups:
-                queryset = Store.objects.filter(user=self.request.user)
-        else:
-            queryset = (
-                Store.objects.annotate(product_count=Count("product"))
-                .filter(product_count__gt=0)
-                .prefetch_related("user__groups")
-                .select_related("user", "address")
-            )
+        queryset = (
+            Store.objects.annotate(product_count=Count("product"))
+            .filter(product_count__gt=0)
+            .prefetch_related("user__groups")
+            .select_related("user", "address")
+        )
         return queryset
 
     def get_permissions(self):
-        if self.action in ["list", "retrieve", "create"]:
+        user_groups = self.get_user_groups()
+        if self.action == "my_store":
+            if "Store Owner" not in user_groups:
+                raise PermissionDenied("You must own a store!")
+        if self.action == "create":
+            if self.request.user.is_staff:
+                raise PermissionDenied("Admins cannot create a store!")
+            if "Store Owner" in user_groups:
+                raise PermissionDenied("You're already a store owner!")
+            self.permission_classes = [IsAuthenticated]
+        if self.action in ["list", "retrieve"]:
             self.permission_classes = [AllowAny]
         if self.action in ["partial_update", "update", "destroy"]:
             self.permission_classes = [IsStoreOwner | IsAdminUser]
@@ -51,11 +61,8 @@ class StoreViewSet(ModelViewSet):
 
     @action(detail=False, methods=["GET"])
     def my_store(self, request):
-        user_groups = self.get_user_groups()
-        if "Store Owner" in user_groups:
-            store = Store.objects.get(user=request.user)
-            return Response(self.get_serializer(store).data, status=status.HTTP_200_OK)
-        raise PermissionDenied()
+        store = Store.objects.get(user=request.user)
+        return Response(self.get_serializer(store).data, status=status.HTTP_200_OK)
 
 
 class CategoryViewSet(ModelViewSet):
@@ -66,28 +73,22 @@ class CategoryViewSet(ModelViewSet):
         user = self.request.user
         queryset = Category.objects.prefetch_related(
             Prefetch("store__user__groups")
-        ).select_related("store__user")
+        ).select_related("store__user", "store__address")
         if user.is_staff:
-            if self.action == "create":
-                raise PermissionDenied("Admins are not allowed to create categories.")
             return queryset
         return queryset.filter(store__user=user)
 
     def get_permissions(self):
-        user = self.request.user
-        user_groups = self.get_user_groups()
-        if "Store Owner" not in user_groups and not user.is_staff:
-            raise PermissionDenied()
-        if user.is_staff and self.action == "create":
-            raise PermissionDenied()
+        if self.action == "create":
+            user_groups = self.get_user_groups()
+            if "Store Owner" not in user_groups:
+                raise PermissionDenied()
+        if self.action in ["partial_update", "update", "destroy"]:
+            self.permission_classes = [IsCategoryOwner | IsAdminUser]
         return super().get_permissions()
 
     def get_serializer_context(self):
-        return (
-            {"user": self.request.user}
-            if self.action == "create"
-            else super().get_serializer_context()
-        )
+        return {"user": self.request.user}
 
     def get_user_groups(self):
         user = self.request.user
@@ -98,13 +99,74 @@ class CategoryViewSet(ModelViewSet):
         return user_groups
 
 
-# class ProductViewSet(ModelViewSet):
-#     queryset = Product.objects.select_related("store")
-#     serializer_class = ProductSerializer
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.prefetch_related(
+        Prefetch("store__user__groups")
+    ).select_related("store__user", "store__address", "category")
+    serializer_class = ProductSerializer
+    pagination_class = None
 
-#     def get_permissions(self):
-#         if self.action in ["list", "retrieve"]:
-#             self.permission_classes = [AllowAny]
-#         if self.action in ["partial_update", "update", "destroy"]:
-#             self.permission_classes = [IsStoreOwner]
-#         return super().get_permissions()
+    def get_permissions(self):
+        if self.action in ["create", "my_products"]:
+            user_groups = self.get_user_groups()
+            if "Store Owner" not in user_groups:
+                raise PermissionDenied()
+        if self.action in ["list", "retrieve"]:
+            self.permission_classes = [AllowAny]
+        if self.action in ["partial_update", "update", "destroy"]:
+            self.permission_classes = [IsProductOwner | IsAdminUser]
+        return super().get_permissions()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve", "my_products"]:
+            self.serializer_class = ListAndRetrieveProductSerializer
+        return super().get_serializer_class()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        product = serializer.save()
+        response_serializer = ListAndRetrieveProductSerializer(
+            product, context=self.get_serializer_context()
+        )
+        headers = self.get_success_headers(response_serializer.data)
+        return Response(
+            response_serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        # Serialize again using ListAndRetrieveProductSerializer for the response
+        response_serializer = ListAndRetrieveProductSerializer(
+            instance, context=self.get_serializer_context()
+        )
+
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
+
+    def get_serializer_context(self):
+        return {"user": self.request.user}
+
+    def get_user_groups(self):
+        user = self.request.user
+        user_groups = getattr(user, "_cached_groups", None)
+        if user_groups is None:
+            user_groups = set(user.groups.values_list("name", flat=True))
+            setattr(user, "_cached_groups", user_groups)
+        return user_groups
+
+    @action(detail=False, methods=["GET"])
+    def my_products(self, request):
+        products = self.get_queryset().filter(store__user=request.user)
+        return Response(
+            self.get_serializer(products, many=True).data, status=status.HTTP_200_OK
+        )
